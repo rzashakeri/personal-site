dd7f3569e41918a0a7f79a19810845f7635afd30
Reza shakeri <rzashakeri@gmail.com>
2023-06-28 10:37:32 +0330

    ✨ feat: added caches system

--- config/settings.py
+++ config/settings.py
@@ -294,3 +294,16 @@ CKEDITOR_CONFIGS = {
 		]),
 	}
 }
+
+CACHES = {
+	"default": {
+		"BACKEND": "django_redis.cache.RedisCache",
+		"LOCATION": env("REDIS_URL"),
+		"OPTIONS": {
+			"CLIENT_CLASS": "django_redis.client.DefaultClient",
+			# Mimicing memcache behavior.
+			# https://github.com/jazzband/django-redis#memcached-exceptions-behavior
+			"IGNORE_EXCEPTIONS": True,
+		},
+	}
+}
