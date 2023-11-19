34c3c4f5266148a7f97b882aac2bb3f36974b93a
Reza shakeri <rzashakeri@gmail.com>
2023-11-20 02:11:45 +0330

    ✨ feat: Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model
+    """
+    title = models.CharField(max_length=100)
+    content = RichTextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-pub_date']
