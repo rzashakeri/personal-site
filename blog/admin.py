683087f94164789fae63712413f6192fe4cb10a8
Reza shakeri <rzashakeri@gmail.com>
2023-11-27 06:54:29 +0330

    ✨ feat: Add Comment Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
 
 
 @admin.register(Post)
@@ -16,4 +16,12 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ["name"]
     ordering = ["name"]
     search_fields = ["name"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["post", "name", "email", "created_at", "updated_at"]
+    list_filter = ["created_at"]
+    ordering = ["created_at"]
+    search_fields = ["content"]
 
