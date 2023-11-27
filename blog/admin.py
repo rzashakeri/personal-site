bd0dfba2a11bce96916b80979fd52c4252cfc988
Reza shakeri <rzashakeri@gmail.com>
2023-11-27 06:49:01 +0330

    ✨ feat: Add TagAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post, Tag
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "created_at", "updated_at"]
     list_filter = ["created_at"]
     ordering = ["created_at"]
     search_fields = ["title"]
 
-admin.site.register(Post, PostAdmin)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    ordering = ["name"]
+    search_fields = ["name"]
+
