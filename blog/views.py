b0b49d97946349691832416526dc8653b809707e
Reza shakeri <rzashakeri@gmail.com>
2023-11-28 00:05:37 +0330

    ✨ feat: Add Tag View

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views import View
 from .models import Post
 
@@ -8,3 +8,10 @@ class PostView(View):
         post = Post.objects.get(slug=slug)
         context = {"page": post}
         return render(request, "blog/post.html", context=context)
+
+
+class TagView(View):
+    def get(self, request, name):
+        posts = get_list_or_404(Post, tags__name=name)
+        context = {"posts": posts}
+        return render(request, "blog/tag.html", context=context)
