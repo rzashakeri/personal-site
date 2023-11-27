5921f1bc860061a0e768ffba8f8e63e87bce28a9
Reza shakeri <rzashakeri@gmail.com>
2023-11-28 00:05:50 +0330

    ✨ feat: Add Tag url

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import PostView
+from blog.views import PostView, TagView
 
 urlpatterns = [
-    path('post/<slug:slug>/', PostView.as_view(), name="post")
+    path('post/<slug:slug>/', PostView.as_view(), name="post"),
+    path('tag/<str:name>/', TagView.as_view(), name="tag"),
 ]
