537a9256d69e8ffe3808a706beb0c011c6f74348
Reza shakeri <rzashakeri@gmail.com>
2023-11-27 18:06:32 +0330

    ♻️ refactor: Delete model Comment

--- blog/models.py
+++ blog/models.py
@@ -33,21 +33,3 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ['name']
-
-
-class Comment(models.Model):
-    """
-    Comment model
-    """
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['-created_at']
