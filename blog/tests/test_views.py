000c27d4df436501952e823a6b7e7a898b8a0c07
Reza shakeri <rzashakeri@gmail.com>
2023-11-29 02:17:27 +0330

    ✅ test: Add TagViewTest

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -39,3 +39,41 @@ class PostViewTest(TestCase):
         # Check if the response status code is 404 (Not Found)
         self.assertEqual(response.status_code, 404)
 
+
+class TagViewTest(TestCase):
+    def setUp(self):
+        # Create a sample tag and posts for testing
+        self.tag = Tag.objects.create(name="Test Tag")
+        self.post1 = Post.objects.create(title="Post 1", content="This is post 1.", slug="post-1")
+        self.post2 = Post.objects.create(title="Post 2", content="This is post 2.", slug="post-2")
+
+        # Associate the posts with the sample tag
+        self.post1.tags.add(self.tag)
+        self.post2.tags.add(self.tag)
+
+    def test_tag_view(self):
+        # Get the URL for the TagView with the sample tag's name
+        url = reverse('tag', kwargs={'name': self.tag.name})
+
+        # Simulate a GET request to the TagView
+        response = self.client.get(url, follow=True)
+
+        # Check if the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the tag name is present in the context
+        self.assertIn('posts', response.context)
+
+        # Check if the rendered HTML contains the post titles
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post2.title)
+
+    def test_tag_view_with_invalid_name(self):
+        # Get a URL with an invalid tag name that does not exist in the database
+        url = reverse('tag', kwargs={'name': 'non-existent-tag'})
+
+        # Simulate a GET request to the TagView with an invalid tag name
+        response = self.client.get(url, follow=True)
+
+        # Check if the response status code is 404 (Not Found)
+        self.assertEqual(response.status_code, 404)
