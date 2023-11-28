73ebf1d31b02b186352455bce77adbfcbb968e65
Reza shakeri <rzashakeri@gmail.com>
2023-11-28 06:10:19 +0330

    ♻️ refactor: refactor PostView and TagView

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.views import View
 from .models import Post
 
 
 class PostView(View):
+    """
+    Post View
+    """
     def get(self, request, slug):
-        post = Post.objects.get(slug=slug)
+        # Retrieve the post object with the given slug from the database
+        post = get_object_or_404(Post, slug=slug)
+
+        # Create a context dictionary with the post object
         context = {"page": post}
+
+        # Render the blog/post.html template with the context data
         return render(request, "blog/post.html", context=context)
 
 
 class TagView(View):
+    """
+    Tag View
+    """
     def get(self, request, name):
+        # Retrieve posts with the specified tag name
         posts = get_list_or_404(Post, tags__name=name)
+
+        # Create a context dictionary with the retrieved posts
         context = {"posts": posts}
+
+        # Render the tag.html template with the context data
         return render(request, "blog/tag.html", context=context)
