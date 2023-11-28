a9913123fc63ab8fd296679681a6e55c531c567c
Reza shakeri <rzashakeri@gmail.com>
2023-11-29 02:42:03 +0330

    ✅ test: Add ContactUsModelTest

--- pages/tests/test_models.py
+++ pages/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
-from pages.models import SiteSettings, Page, About
+from pages.models import SiteSettings, Page, About, ContactUs
 
 
 class SiteSettingsModelTest(TestCase):
@@ -26,4 +26,20 @@ class AboutModelTest(TestCase):
         page = Page.objects.create(title='Test Page', icon=SimpleUploadedFile("icon.png", b"file_content"))
         about = About.objects.create(page=page, heading='About Me', body='Some details about me.')
         self.assertEqual(str(about), 'About Me')
+
+
+
+class ContactUsModelTest(TestCase):
+    def test_contact_us_str(self):
+        page = Page.objects.create(title='Test Page', icon=SimpleUploadedFile("icon.png", b"file_content"))
+        contact_us = ContactUs.objects.create(
+            page=page,
+            first_name='John',
+            last_name='Doe',
+            email='john@example.com',
+            message='Hello, this is a test message.'
+        )
+        expected_str = f"From: {contact_us.email} | Time: {contact_us.created_on}"
+        self.assertEqual(str(contact_us), expected_str)
+
 
